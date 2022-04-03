8e28d87dd07406513d54f14777899bf6b63ede3a
ELKHAYYAT MOHAMMAD ELAMINE <96497970+Med-Elamine@users.noreply.github.com>
2022-04-03 17:32:26 +0000

    use screenOptions & options props to customize our screens

--- App.js
+++ App.js
@@ -15,12 +15,26 @@ const stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <stack.Navigator initialRouteName='Home'>
-        <stack.Screen name='Home' component={HomeScreen}/>
+      <stack.Navigator initialRouteName='Home' 
+        screenOptions={{
+        headerTintColor: 'white',
+        headerStyle: {backgroundColor: 'orange'}
+      }}>
+        <stack.Screen name='Home' component={HomeScreen}
+        options={{
+          title: 'Movie Time',
+          headerTitleStyle: {fontSize:30, fontWeight: 'bold'}
+        }}/>
         <stack.Screen name='Details' component={DetailScreen}/>
         <stack.Screen name='MoreDetails' component={DetailScreen}/>
-        <stack.Screen name='Movie' component={MovieScreen}/>
-        <stack.Screen name='MoreMovieDetails' component={MovieScreen}/>
+        <stack.Screen name='Movie' component={MovieScreen}
+        options={
+          ({route}) => ({title: route.params.movie.title})
+        }/>
+        <stack.Screen name='MoreMovieDetails' component={MovieScreen}
+        options={
+          ({route}) => ({title: route.params.movie.title})
+        }/>
         <stack.Screen name='BigImage' component={ImageScreen}/>
       </stack.Navigator>
     </NavigationContainer>
