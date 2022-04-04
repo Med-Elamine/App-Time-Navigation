5e53b27091fcc47955ee7c0b6e507bbe98c79e8b
ELKHAYYAT MOHAMMAD ELAMINE <96497970+Med-Elamine@users.noreply.github.com>
2022-04-04 09:49:51 +0000

    use the Touchable API

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text, StyleSheet,TouchableOpacity } from 'react-native';
 
 const HomeScreen = ({navigation, route}) => {
   console.log(route);
     return (
       <View style={styles.mainView}>
         <Text>Home Screen</Text>
-          <Button
-            title="Go to Details"
-            onPress={() => {navigation.navigate("Details",{from: "Home",screenNumber: 1})}}/>
+          <TouchableOpacity
+            style={styles.touchableView}
+            onPress={() => {navigation.navigate("Details",{from: "Home",screenNumber: 1})}}>
+              <Text style={styles.touchableText}>Go to Details</Text>
+            </TouchableOpacity>
             <Text>Movies</Text>
             <Button
           title="Star Wars"
@@ -43,6 +45,23 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems: "center",
       justifyContent: "center",
+    },
+    touchableView: {
+      marginTop: 30,
+      marginBottom: 30,
+      width: 150,
+      height: 50,
+      alignItems: 'center',
+      backgroundColor: 'orange',
+      borderWidth: 5,
+      borderRadius: 10,
+    },
+    touchableText: {
+      textAlign: 'center',
+      padding: 5,
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white'
     }
   });
   
